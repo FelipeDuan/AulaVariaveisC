a1df659bd77c2bbe929c8267a8985b75f6ba31f8
Felipe Duan <felipeduan1@gmail.com>
2024-04-23 11:32:08 -0300

    finalizando a aula

--- Programa1.C
+++ Programa1.C
@@ -1,7 +1,21 @@
 // Aula sobre Variavéis
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
 int main() {
+    // Declarando Varíaveis
+    int Idade;
+
+    // Declarando e iniacializando variáveis
+    //int Idade = 0;
+
+    // Função para escrever algo na saída padrão (console)
+    printf("Qual a sua idade? \n");
+    scanf("%d", &Idade); //Receber dados para guardar na variável
+
+    // Saída de Dados
+    printf("A sua idade é %d", Idade);
 
     return 0;
 }
